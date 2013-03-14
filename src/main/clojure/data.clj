5fa3b878aa5811814419aad34a5730164c7f3a8c
Kjetil Valstadsve <valstadsve@gmail.com>
2013-03-14 22:36:24 +0100

    Messin' about.

--- src/main/clojure/data.clj
+++ src/main/clojure/data.clj
@@ -20,30 +20,93 @@
           attribute-names (map normalize accessed-names)]
       (zipmap method-names attribute-names))))
 
+;(defmacro reifier-body [builder-class target-class instance]
+;  (let [->instance (symbol (str "map->" (. target-class getSimpleName)))
+;        set-method (fn [[methodName attrName]]
+;                     `(~(symbol methodName) [this value] 
+;                        (reifier-body ~builder-class ~target-class (assoc ~instance ~(keyword attrName) ~value))))
+;        get-method (fn [[methodName attrName]]
+;                     `(~(symbol methodName) [this]
+;                        (~(keyword attrName) ~instance)))
+;        set-methods (map set-method set-meth-attrs)
+;        get-methods (map get-method get-meth-attrs)]
+;    `(reify ~builder-class
+;       ~@set-methods
+;       (toString [this] (str ~instance)) 
+;       (build [this] (reify target-class
+;                       ~@get-methods
+;                       (toString [this] (str ~instance)))))))
+;
+;(defmacro defbuilder [builder-class-symbol]
+;  (let [builder-class (eval builder-class-symbol)
+;        target-class (built-class builder-class)
+;        target-class-symbol (symbol (. target-class getName))
+;        instance (gensym)
+;        ]
+;    (refifier-body builder-class target-class nil)))
+
 (defmacro defbuilder [builder-class-symbol]
   (let [builder-class (eval builder-class-symbol)
         target-class (built-class builder-class)
         target-class-symbol (symbol (. target-class getName))
         set-meth-attrs (seq (method-attributes builder-class))
-        get-meth-attrs (seq (method-attributes target-class))
         instance (gensym)
         set-method (fn [[methodName attrName]]
-                     (let [this (gensym)
+                     (let [this (gensym) 
                            value (gensym)]
-                       `(~(symbol methodName) [~this ~value] (do (def ~instance (assoc ~instance ~attrName ~value)) ~this))))
+                       `(~(symbol methodName) [~this ~value] 
+                          (do 
+                            (def ~instance (assoc ~instance ~(keyword attrName) ~value))
+                            ~this))))
+        set-methods (map set-method set-meth-attrs)
+        get-meth-attrs (seq (method-attributes target-class))
         get-method (fn [[methodName attrName]]
                      (let [this (gensym)]
-                       `(~(symbol methodName) [~this] (get ~instance ~attrName))))
-        set-methods (map set-method set-meth-attrs)
-        get-methods (map get-method get-meth-attrs)]
+                       `(~(symbol methodName) [~this]
+                          (~(keyword attrName) ~instance))))
+        get-methods (map get-method get-meth-attrs)
+        mapctr (symbol (str "map->" (. target-class getSimpleName)))]
     `(do 
-       (def ~instance (~(symbol (str 'map-> (. target-class getSimpleName))) {})) 
+       (def ~instance (~mapctr {})) 
        (reify ~builder-class-symbol
          ~@set-methods
-         (toString [this] (str ~instance))
+         (toString [this] (. ~instance toString))
          (build [this] (reify ~target-class-symbol 
                          ~@get-methods
-                         (toString [this] (str ~instance))))))))
+                         (toString [this] (. ~instance toString))))))))
+
+(defmacro type-instance [type-name type-instance]
+  (let [target-class (eval type-name)
+        get-meth-attrs (seq (method-attributes target-class))
+        get-method (fn [[methodName attrName]]
+                     (let [this (gensym)]
+                       `(~(symbol methodName) [~this]
+                          (~(keyword attrName) ~type-instance))))
+        get-methods (map get-method get-meth-attrs)]
+    `(reify ~type-name 
+       ~@get-methods
+       (toString [this] (. ~type-instance toString)))))
+
+(defmacro type-builder [builder-class-symbol]
+  (let [builder-class (eval builder-class-symbol)
+        target-class (built-class builder-class)
+        target-class-symbol (symbol (. target-class getName))
+        set-meth-attrs (seq (method-attributes builder-class))
+        instance (gensym)
+        set-method (fn [[methodName attrName]]
+                     (let [this (gensym) 
+                           value (gensym)]
+                       `(~(symbol methodName) [~this ~value] 
+                          (do 
+                            (def ~instance (assoc ~instance ~(keyword attrName) ~value))
+                            ~this))))
+        set-methods (map set-method set-meth-attrs)
+        mapctr (symbol (str "map->" (. target-class getSimpleName)))]
+    `(fn [~instance]  
+       (reify ~builder-class-symbol
+         ~@set-methods
+         (toString [this] (. ~instance toString))
+         (build [this] (type-instance ~target-class-symbol ~instance))))))
 
 (defmacro defdom [& names]
   (letfn [(declared-fields-vector [intf]
@@ -61,12 +124,12 @@
   (letfn [(setter [smb]
             (let [strng (str smb)]
               (symbol (str "set" (str/capitalize (first strng)) (str/join "" (rest strng))))))
-           (to-call [[attribute value]] 
+          (to-call [[attribute value]] 
             `(. ~builder ~(setter attribute) ~value))]
     `(do
        ~@(map to-call (partition 2 methodCalls))
        (. ~builder build))))
-    
+
 (defdom
   jdata.examples.PersonBuilder
   jdata.examples.NameBuilder
@@ -84,12 +147,12 @@
                  (new Address "Nonnegata" "21" "0656" "Oslo"))]
     (println person))
   (println (macroexpand-1 '(defbuilder jdata.examples.NameBuilder)))
-  (let [bldr (defbuilder jdata.examples.NameBuilder)
-        kjetil (build bldr
+  (let [kjetil (build (defbuilder jdata.examples.NameBuilder)
                  [firstName "Kjetil"
                   middleName "Jamne"
                   lastName "Valstadsve"])]
-    (println kjetil)
+    (println (. kjetil toString))
     (println (type kjetil))
-    (println (. kjetil getFirstName))))
+    (println (. kjetil getFirstName))
+    (println (:firstName kjetil))))
 (-main)
